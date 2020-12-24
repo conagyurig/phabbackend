b0e605cb4cea235e8be75fe841ae64c0799dac29
conagyurig <youngcraig23@gmail.com>
2020-12-24 15:28:05 +0000

    Initial commit

---
+++ src/main/java/MyServlet.java
@@ -0,0 +1,26 @@
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+@WebServlet(urlPatterns={"/tables"},loadOnStartup = 1)
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        SqlSetup tables = new SqlSetup();
+        resp.getWriter().write("<html> <head> <title>CMDMC</title> </head><body> <h1>Name from database: Craig </h1> </body> </html>");
+        resp.getWriter().write(req.getServletPath());
+    }
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+
+
+    }
+
+}
