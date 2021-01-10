44b9cc1095dc64b98ecbfe244ec2ccffb0b68bed
conagyurig <youngcraig23@gmail.com>
2021-01-10 17:14:16 +0000

    working on customer return again

--- src/main/java/AccessClasses/Customers.java
+++ src/main/java/AccessClasses/Customers.java
@@ -1,11 +1,12 @@
 package AccessClasses;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Customers implements Serializable {
     List<Customer> customers;
     public Customers(){
-
+        customers = new ArrayList<>();
     }
     public void addCustomers(Customer customer) {
         this.customers.add(customer);
